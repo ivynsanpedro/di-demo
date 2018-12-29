ade4e30df30fbc8d83cdce24fdbe88091da3f654
Ivyn San Pedro <isanpedro@gmail.com>
2018-12-29 08:37:39 -0500

    Add controller

--- src/main/java/sanpedro/ivyn/didemo/DiDemoApplication.java
+++ src/main/java/sanpedro/ivyn/didemo/DiDemoApplication.java
@@ -2,12 +2,18 @@ package sanpedro.ivyn.didemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import sanpedro.ivyn.didemo.controllers.MyController;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiDemoApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
+
+		MyController controller = (MyController) ctx.getBean("myController");
+
+		controller.hello();
 	}
 
 }

66fbe71798745452b05174b9d82914db425ac659
Ivyn San Pedro <isanpedro@gmail.com>
2018-12-28 21:42:42 -0500

    Initial commit

---
+++ src/main/java/sanpedro/ivyn/didemo/DiDemoApplication.java
@@ -0,0 +1,14 @@
+package sanpedro.ivyn.didemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiDemoApplication.class, args);
+	}
+
+}
+
